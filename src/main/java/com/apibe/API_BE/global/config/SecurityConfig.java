147ff6a1c1ad041b8673b4be1e9fabbc478b1243
BiM1406 <datdif@gmail.com>
2026-06-15 21:21:04 +0700

    refactor: remove MockAuthenticationFilter and its test

--- src/main/java/com/apibe/API_BE/global/config/SecurityConfig.java
+++ src/main/java/com/apibe/API_BE/global/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.apibe.API_BE.global.config;
 
-import com.apibe.API_BE.global.security.MockAuthenticationFilter;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,18 +11,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-
-    private final ObjectProvider<MockAuthenticationFilter> mockAuthenticationFilterProvider;
-
-    public SecurityConfig(ObjectProvider<MockAuthenticationFilter> mockAuthenticationFilterProvider) {
-        this.mockAuthenticationFilterProvider = mockAuthenticationFilterProvider;
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -40,11 +31,6 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             );
-
-        MockAuthenticationFilter mockFilter = mockAuthenticationFilterProvider.getIfAvailable();
-        if (mockFilter != null) {
-            http.addFilterBefore(mockFilter, UsernamePasswordAuthenticationFilter.class);
-        }
 
         return http.build();
     }
